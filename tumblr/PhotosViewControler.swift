99c9f001d6ed0db7aef5be4cae58814ff2e6b4b0
Hunter Boleman <hunter.boleman@gmail.com>
2019-02-24 15:48:01 -0800

    Milestone 1: complete

--- tumblr/PhotosViewControler.swift
+++ tumblr/PhotosViewControler.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PhotosViewControler.swift
 //  tumblr
 //
 //  Created by Hunter Boleman on 2/24/19.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotosViewControler: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
-
 }
-
